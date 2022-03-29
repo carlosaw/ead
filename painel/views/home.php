ee17a2e78199da47f7d0431c37aaa3f50e5b4317
unknown <carlosfreevg@gmail.com>
2022-03-28 21:49:48 -0400

    LayoutBlack

--- painel/views/home.php
+++ painel/views/home.php
@@ -1,6 +1,8 @@
-<h1 style="text-align:center">Cursos</h1>
 
-<button class="btn"><a href="<?php echo BASE; ?>home/adicionar">Adicionar Curso</a><br/></button><hr/>
+	<h1 style="text-align:center">Cursos</h1>
+	
+
+
 <table border="0" width="100%">
 	<tr>
 		<th class="ptr">Imagem</th>
@@ -11,18 +13,20 @@
 
 	<?php foreach($cursos as $curso): ?>
 		<tr>
-			<td class="ptr" width="150"><img src="<?php echo BASE; ?>../assets/images/cursos/<?php echo $curso['imagem']; ?>" border="0" width="200" /></td>
+			<td class="ptr" width="130"><img src="<?php echo BASE; ?>../assets/images/cursos/<?php echo $curso['imagem']; ?>" border="0" width="200" /></td>
 			
 			<td class="ptrname"><?php echo $curso['nome']; ?></td>
 			
 			<td class="ptr" align="center"><?php echo $curso['qtalunos']; ?></td>
 			
 			<td class="ptr" width="200">
-			<div class="btnAction">
-				<button class="btn b"><a href="<?php echo BASE; ?>home/editar/<?php echo $curso['id']; ?>">Editar</a></button>
-			
-				<button class="btn c"><a href="<?php echo BASE; ?>home/excluir/<?php echo $curso['id']; ?>">Excluir</a></button>
-			</div>
+				<div class="btnAction">
+					<button class="btn a"><a href="<?php echo BASE; ?>home/adicionar">Add </a><br/></button>
+
+					<button class="btn b"><a href="<?php echo BASE; ?>home/editar/<?php echo $curso['id']; ?>">Edit</a></button>
+				
+					<button class="btn c"><a href="<?php echo BASE; ?>home/excluir/<?php echo $curso['id']; ?>">Del</a></button>
+				</div>
 			</td>
 		</tr>
 	<?php endforeach; ?>
