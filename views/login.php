a4a7a575eea677bf1016ddafa91720c5117666fd
carlosaw <carlosfreevg@gmail.com>
2024-04-12 21:24:00 -0400

    3/16

--- views/login.php
+++ views/login.php
@@ -10,9 +10,7 @@
 		<form method="POST" class="form_login">
 			<h2>Bem Vindo ao EAD</h2>
 			<input type="email" name="email" placeholder="E-mail" autofocus/><br/><br/>
-
 			<input type="password" name="senha" placeholder="Senha"/><br/><br/>
-
 			<input type="submit" value="Entrar"/>
 		</form>
 	</body>
